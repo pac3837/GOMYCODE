ac9ccc4ae8daf21f98ef2dadcda294acd32d86d7
pac3837 <86604378+pac3837@users.noreply.github.com>
2021-07-25 17:29:04 +0100

    Add files via upload

---
+++ check-point-10.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+mylist=[14, "hello", 967]
+mylist[2]
+
+
+# In[3]:
+
+
+import pandas
+import numpy
+syntax error
+
+
+# In[4]:
+
+
+print("python errors")
+python errors
+key error
+
+
+# In[5]:
+
+
+mydictionnary={True:"hello",False:"bye", '3':"python"}
+mydictionnary[True]
+
+
+# In[6]:
+
+
+i=14
+while i<78:
+    print(i)
+    i+=1
+
+
+# In[7]:
+
+
+it=iter([1,2,3])
+next(it)
+
+
+# In[8]:
+
+
+15+15
+
+
+# In[9]:
+
+
+str('python')
+
+
+# In[10]:
+
+
+python = 2
+print(python)
+
+
+# In[11]:
+
+
+x=19/2
+print(x)
+
+
+# In[ ]:
+
+
+
+
